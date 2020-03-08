7c38a836434340f1efb13e3ec8f9367dc74d511d
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-03-08 13:01:26 -0300

    feat(hex): create Hex Metrics script
    - this class wraps all math metrics needed to create hex, like
    innerRadius, outerRadius and the vertex corners positions.

---
+++ Assets/Scripts/Hex/HexMetrics.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HexMetrics : MonoBehaviour
+{
+    public const float outerRadius = 10f;
+
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+	public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),						// vertex 1
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),		// vertex 2
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),		// vertex 3
+		new Vector3(0f, 0f, -outerRadius),						// vertex 4
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),		// vertex 5
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius)		// vertex 6
+	};
+}
