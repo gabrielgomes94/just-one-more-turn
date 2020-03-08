01bb69dd83d3da8f2876d2385e41dbcc07791b4a
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-03-08 13:04:03 -0300

    feat(hex): add hex grid
    - creates a class that will be responsible for generating an hex grid
    - also, creates a hex cell class that will be responsible for handling cells information,
    like position, coordinates and another cell's properties and information.

---
+++ Assets/Scripts/Hex/HexGrid.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+	public int width = 6;
+	public int height = 6;
+
+	public HexCell cellPrefab;
+
+	HexCell[] cells;
+
+	void Awake()
+	{
+		cells = InstantiateHexCellsArray();
+
+		CreateCells();
+	}
+
+	void CreateCell(int x, int z, int i)
+	{
+		Vector3 position = GetCellPosition(x, z);
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+	}
+
+	private HexCell[] InstantiateHexCellsArray()
+    {
+		return new HexCell[height * width];
+	}
+
+	private void CreateCells()
+    {
+		for (int z = 0, i = 0; z < height; z++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+
+	private Vector3 GetCellPosition(int x, int z)
+    {
+		Vector3 position;
+		position.x = x * 10f;
+		position.y = 0f;
+		position.z = z * 10f;
+
+		return position;
+	}
+}
