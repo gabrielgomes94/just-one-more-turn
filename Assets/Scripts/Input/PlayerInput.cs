074fbfa22592e87d77da2c5a0246e22f296cc8d1
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-04-02 00:57:30 -0300

    feat(input): create actions for left and mouse button
    - each input action must have a method to handle the data inputted

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,25 +6,45 @@ public class PlayerInput : MonoBehaviour
 {
     public InputAction primaryAction;
     public InputAction secondaryAction;
+    public InputAction mousePosition;
+    private Vector3 vectorMousePosition;
+    private PrimaryActionHandler primaryActionHandler;
 
     private void Awake ()
     {
-        primaryAction.performed += Click;
-        secondaryAction.performed += Click;
+        primaryActionHandler = new PrimaryActionHandler();
+
+        primaryAction.performed += primaryActionClick;
+        secondaryAction.performed += secondaryActionClick;
+        mousePosition.performed += trackMousePosition;
     }
 
     private void OnEnable() {
         primaryAction.Enable();
         secondaryAction.Enable();
+        mousePosition.Enable();
     }
 
     private void OnDisable() {
         primaryAction.Disable();
         secondaryAction.Disable();
+        mousePosition.Disable();
     }
 
-    private void Click(InputAction.CallbackContext context)
+    private void primaryActionClick(InputAction.CallbackContext context)
     {
-        Debug.Log("Clicou");
+        Debug.Log("Clicou com o esquerdo - " + this.vectorMousePosition);
+        primaryActionHandler.handle(this.vectorMousePosition);
     }
+
+    private void secondaryActionClick(InputAction.CallbackContext context)
+    {
+        Debug.Log("Clicou com o direito - " + this.vectorMousePosition);
+    }
+
+    private void trackMousePosition(InputAction.CallbackContext context)
+    {
+        this.vectorMousePosition = context.ReadValue<Vector2>();
+    }
+
 }
