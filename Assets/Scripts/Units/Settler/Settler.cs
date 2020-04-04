1952078e631a8168e35ab42a862da25eeee37a04
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-04-04 14:04:09 -0300

    feat(settler): implements selection and show/hide panel
    - select method triggers the method responsable to show/hide the actions panel.

--- Assets/Scripts/Units/Settler/Settler.cs
+++ Assets/Scripts/Units/Settler/Settler.cs
@@ -6,13 +6,29 @@ public class Settler : MonoBehaviour, IInteractable
 {
     public SettlerData settlerData;
 
+    public bool isSelected { get; set; }
+
+    public GameObject gameObject;
     public Settler()
     {
         settlerData = new SettlerData();
     }
 
-    public void Selection()
+    public void Awake()
     {
-        Debug.Log("Settler ==========|| interface");
+        this.isSelected = false;
+        ShowActionsPanel();
+    }
+
+    public void Select()
+    {
+        this.isSelected = (this.isSelected == true) ? false : true;
+
+        ShowActionsPanel();
+    }
+
+    private void ShowActionsPanel()
+    {
+        gameObject.GetComponentInChildren<Canvas>().enabled = this.isSelected;
     }
 }
