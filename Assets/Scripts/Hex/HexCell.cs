b13edd8746a3180904a2ab602e77dedfd0ef142f
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-04-22 05:38:22 -0300

    refact(hex): add hex namespace

--- Assets/Scripts/Hex/HexCell.cs
+++ Assets/Scripts/Hex/HexCell.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HexCell : MonoBehaviour
+namespace Hex
 {
-    public HexCoordinates coordinates;
+    public class HexCell : MonoBehaviour
+    {
+        public HexCoordinates coordinates;
+    }
 }
--- Assets/Scripts/Hex/HexMesh.cs
+++ Assets/Scripts/Hex/HexMesh.cs
@@ -1,76 +1,79 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-public class HexMesh : MonoBehaviour, IInteractable
+namespace Hex
 {
-    Mesh hexMesh;
-    List<Vector3> vertices;
-    List<int> triangles;
-
-    MeshCollider meshCollider;
-
-    void Awake()
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class HexMesh : MonoBehaviour, IInteractable
     {
-        GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
-        meshCollider = gameObject.AddComponent<MeshCollider>();
-        hexMesh.name = "Hex Mesh";
+        Mesh hexMesh;
+        List<Vector3> vertices;
+        List<int> triangles;
 
-        vertices = new List<Vector3>();
-        triangles = new List<int>();
-    }
+        MeshCollider meshCollider;
 
-    public void Triangulate(HexCell[] cells)
-    {
-        ClearMesh();
+        void Awake()
+        {
+            GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+            meshCollider = gameObject.AddComponent<MeshCollider>();
+            hexMesh.name = "Hex Mesh";
 
-        for (int i = 0; i < cells.Length; i++) {
-            Triangulate(cells[i]);
+            vertices = new List<Vector3>();
+            triangles = new List<int>();
         }
 
-        hexMesh.vertices = vertices.ToArray();
-        hexMesh.triangles = triangles.ToArray();
-        hexMesh.RecalculateNormals();
+        public void Triangulate(HexCell[] cells)
+        {
+            ClearMesh();
 
-        meshCollider.sharedMesh = hexMesh;
-    }
+            for (int i = 0; i < cells.Length; i++) {
+                Triangulate(cells[i]);
+            }
 
-    private void Triangulate(HexCell cell)
-    {
-        Vector3 center = cell.transform.localPosition;
-        for (int i = 0; i < 6; i++) {
-            AddTriangle(
-                center,
-                center + HexMetrics.corners[i],
-                center + HexMetrics.corners[i + 1]
+            hexMesh.vertices = vertices.ToArray();
+            hexMesh.triangles = triangles.ToArray();
+            hexMesh.RecalculateNormals();
 
-            );
+            meshCollider.sharedMesh = hexMesh;
         }
-    }
 
-    void AddTriangle(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3)
-    {
-        int vertexIndex = vertices.Count;
+        private void Triangulate(HexCell cell)
+        {
+            Vector3 center = cell.transform.localPosition;
+            for (int i = 0; i < 6; i++) {
+                AddTriangle(
+                    center,
+                    center + HexMetrics.corners[i],
+                    center + HexMetrics.corners[i + 1]
 
-        vertices.Add(vertex1);
-        vertices.Add(vertex2);
-        vertices.Add(vertex3);
+                );
+            }
+        }
 
-        triangles.Add(vertexIndex);
-        triangles.Add(vertexIndex + 1);
-        triangles.Add(vertexIndex + 2);
-    }
+        void AddTriangle(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3)
+        {
+            int vertexIndex = vertices.Count;
 
-    private void ClearMesh()
-    {
-        hexMesh.Clear();
-        vertices.Clear();
-        triangles.Clear();
-    }
+            vertices.Add(vertex1);
+            vertices.Add(vertex2);
+            vertices.Add(vertex3);
 
-    public bool isSelected { get; set; }
-    public void Select()
-    {
-        Debug.Log("Mapa ==========|| interface");
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
+        }
+
+        private void ClearMesh()
+        {
+            hexMesh.Clear();
+            vertices.Clear();
+            triangles.Clear();
+        }
+
+        public bool isSelected { get; set; }
+        public void Select()
+        {
+            Debug.Log("Mapa ==========|| interface");
+        }
     }
 }
