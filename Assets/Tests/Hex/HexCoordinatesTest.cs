0e8ef2f6d976235630b41b61ee3a092b7993948e
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-04-23 05:37:17 -0300

    test(hex): add editor tests for hex classes

---
+++ Assets/Tests/Hex/HexCoordinatesTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Hex;
+
+namespace Tests
+{
+    public class HexCoordinatesTest
+    {
+        [Test]
+        public void Instantiate_HexCoordinates_From_Offset_Coordinates()
+        {
+            HexCoordinates hexCoordinates = HexCoordinates.FromOffsetCoordinates(1, 3);
+
+            Assert.AreEqual(hexCoordinates.X, 1 - 3 /2);
+            Assert.AreEqual(hexCoordinates.Z, 3);
+        }
+
+        [Test]
+        public void Test_Get_Coordinates_String_Formatted()
+        {
+            HexCoordinates hexCoordinates = new HexCoordinates(1, 1);
+            string hexCoordinatesText = "(1, -2, 1)";
+
+            Assert.AreEqual(hexCoordinates.ToString(), hexCoordinatesText);
+        }
+
+        [Test]
+        public void Test_Get_Coordinates_String_Formatted_On_SepareteLines()
+        {
+            HexCoordinates hexCoordinates = new HexCoordinates(1, 1);
+            string hexCoordinatesText = "1\n-2\n1";
+
+            Assert.AreEqual(hexCoordinates.ToStringOnSeparateLines(), hexCoordinatesText);
+        }
+    }
+}
---
+++ Assets/Tests/Hex/HexMetricsTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Hex;
+
+namespace Tests
+{
+    public class HexMetricsTest
+    {
+        [Test]
+        public void HexMetricsTestSimplePasses()
+        {
+            Vector3[] corners = HexMetrics.corners;
+
+            Assert.AreEqual(corners[0], new Vector3(0f, 0f, 10f));
+
+            Assert.AreEqual(
+                corners[1],
+                new Vector3(
+                    10f *  0.866025404f,
+                    0f,
+                    0.5f * 10f
+                )
+            );
+
+            Assert.AreEqual(
+                corners[2],
+                new Vector3(
+                    10f *  0.866025404f,
+                    0f,
+                    -0.5f * 10f
+                )
+            );
+
+            Assert.AreEqual(
+                corners[3],
+                new Vector3(
+                    0f,
+                    0f,
+                    -10f
+                )
+            );
+
+            Assert.AreEqual(
+                corners[4],
+                new Vector3(
+                    -10f *  0.866025404f,
+                    0f,
+                    -0.5f * 10f
+                )
+            );
+
+            Assert.AreEqual(
+                corners[5],
+                new Vector3(
+                    -10f *  0.866025404f,
+                    0f,
+                    0.5f * 10f
+                )
+            );
+
+            Assert.AreEqual(corners[6], new Vector3(0f, 0f, 10f));
+        }
+    }
+}
