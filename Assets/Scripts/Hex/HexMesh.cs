0f41759cad9d7108d16a258342d9e042956820bf
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-05-06 01:52:26 -0300

    refactor(input): rename input namespace to game input

--- Assets/Scripts/Hex/HexMesh.cs
+++ Assets/Scripts/Hex/HexMesh.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using Input;
+using GameInput;
 
 namespace Hex
 {
--- Assets/Scripts/Input/ActionHandlers/PrimaryActionHandler.cs
+++ Assets/Scripts/Input/ActionHandlers/PrimaryActionHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Input
+namespace GameInput
 {
     public class PrimaryActionHandler
     {
