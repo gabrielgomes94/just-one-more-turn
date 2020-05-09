3452ec512a0fa31a7ef3d3b10635b12c9ed29abf
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-05-09 00:26:38 -0300

    feat: add hex color class
    - create a class to handle colors on hexes.
    - for now, this class has a static method that retrieves a random valid
    color

---
+++ Assets/Scripts/Hex/HexColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Hex
+{
+    public class HexColor
+    {
+        static Color[] colors = {
+            Color.white,
+            Color.green,
+            Color.yellow,
+            Color.gray,
+        };
+
+        public static Color GetRandomColor()
+        {
+            return colors[Random.Range(0, colors.Length)];
+        }
+    }
+}
