242956af1d096d5226897dc2a9f6b94faebe385d
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-05-26 00:12:39 -0300

    fix: add namespace and import unit collections

--- Assets/Scripts/Hex/Services/TriangulateHexMeshService.cs
+++ Assets/Scripts/Hex/Services/TriangulateHexMeshService.cs
@@ -1,33 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
-public class TriangulateHexMeshService
+namespace Hex
 {
-    public static NativeList<Vector3> AddVertices(NativeList<Vector3> vertices, Vector3 centerPosition, int i)
+    public class TriangulateHexMeshService
     {
-        vertices.Add(centerPosition);
-        vertices.Add(centerPosition + HexMetrics.corners[i]);
-        vertices.Add(centerPosition + HexMetrics.corners[i + 1]);
+        public static NativeList<Vector3> AddVertices(NativeList<Vector3> vertices, Vector3 centerPosition, int i)
+        {
+            vertices.Add(centerPosition);
+            vertices.Add(centerPosition + HexMetrics.corners[i]);
+            vertices.Add(centerPosition + HexMetrics.corners[i + 1]);
 
-        return vertices;
-    }
+            return vertices;
+        }
 
-    public static NativeList<int> AddTriangles(NativeList<int> triangles, int vertexIndex)
-    {
-        triangles.Add(vertexIndex);
-        triangles.Add(vertexIndex + 1);
-        triangles.Add(vertexIndex + 2);
+        public static NativeList<int> AddTriangles(NativeList<int> triangles, int vertexIndex)
+        {
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
 
-        return triangles;
-    }
+            return triangles;
+        }
 
-    public static NativeList<Color> AddColors(NativeList<Color> colors, Color color)
-    {
-        colors.Add(color);
-        colors.Add(color);
-        colors.Add(color);
+        public static NativeList<Color> AddColors(NativeList<Color> colors, Color color)
+        {
+            colors.Add(color);
+            colors.Add(color);
+            colors.Add(color);
 
-        return colors;
+            return colors;
+        }
     }
 }
