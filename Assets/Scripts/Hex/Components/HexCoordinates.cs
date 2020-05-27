1991c4c7d0e185b52387d535c941d841c7c4de06
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-05-27 01:42:14 -0300

    feat(hex): override operator to hex coordinates
    - in order to compare two hex coordinates, I am going to override the
    `!=` and the `==` operator.

--- Assets/Scripts/Hex/Components/HexCoordinates.cs
+++ Assets/Scripts/Hex/Components/HexCoordinates.cs
@@ -7,5 +7,26 @@ namespace Hex {
     public struct HexCoordinates : IComponentData
     {
         public int X, Y, Z;
+
+        public static bool operator == (HexCoordinates hexCoordinates1, HexCoordinates hexCoordinates2) {
+            bool status = false;
+            if (
+                hexCoordinates1.X == hexCoordinates2.X &&
+                hexCoordinates1.Y == hexCoordinates2.Y &&
+                hexCoordinates1.Z == hexCoordinates2.Z
+            ) {
+                status = true;
+            }
+            return status;
+        }
+
+        public static bool operator != (HexCoordinates hexCoordinates1, HexCoordinates hexCoordinates2) {
+            bool status = false;
+            if (hexCoordinates1.X != hexCoordinates2.X) return true;
+            if (hexCoordinates1.Y != hexCoordinates2.Y) return true;
+            if (hexCoordinates1.Z != hexCoordinates2.Z) return true;
+
+            return status;
+        }
     }
 }
