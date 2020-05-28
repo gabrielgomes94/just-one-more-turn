ccee439a137fe0ae7744efb3fc19bc08a5152392
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-05-27 23:30:26 -0300

    feat(hex): add a finder class to get hex cell's data

---
+++ Assets/Scripts/Hex/Entities/Finders/HexCellFinder.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Transforms;
+
+namespace Hex
+{
+    public class HexCellFinder
+    {
+        public static NativeArray<ColorComponent> GetColorsComponentsArray(EntityQuery query)
+        {
+            return query.ToComponentDataArray<ColorComponent>(Allocator.Temp);
+        }
+
+        public static NativeArray<HexCoordinates> GetHexCoordinatesArray(EntityQuery query)
+        {
+            return query.ToComponentDataArray<HexCoordinates>(Allocator.Temp);
+        }
+
+        public static Color GetColorByIndex(EntityQuery query, int index)
+        {
+            NativeArray<ColorComponent> colorComponentsArray = HexCellFinder.GetColorsComponentsArray(query);
+
+            return colorComponentsArray[index].Value;
+        }
+    }
+}
