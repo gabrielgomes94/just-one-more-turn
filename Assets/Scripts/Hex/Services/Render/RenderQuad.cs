743052090eb5d43758e9d01ded1882414aacaca3
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-05-27 23:24:02 -0300

    feat(hex): add classes to render quads and triangles
    - these classes implements an interface that require methods to add
    vertices, triangles and colors that will be rendered.

---
+++ Assets/Scripts/Hex/Services/Render/RenderQuad.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+namespace Hex
+{
+    public class RenderQuad: ITriangulatable
+    {
+        int vertexIndex;
+        Vector3 vertex1;
+        Vector3 vertex2;
+        Vector3 vertex3;
+        Vector3 vertex4;
+        Color color1;
+        Color color2;
+
+        public RenderQuad(
+            int vertexIndex,
+            Vector3 vertex1,
+            Vector3 vertex2,
+            Vector3 vertex3,
+            Vector3 vertex4,
+            Color color1,
+            Color color2
+        ) {
+            this.vertexIndex = vertexIndex;
+
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.vertex3 = vertex3;
+            this.vertex4 = vertex4;
+
+            this.color1 = color1;
+            this.color2 = color2;
+        }
+
+        public NativeList<Vector3> AddVertices (NativeList<Vector3> vertices)
+        {
+            vertices.Add(vertex1);
+            vertices.Add(vertex2);
+            vertices.Add(vertex3);
+            vertices.Add(vertex4);
+
+            return vertices;
+        }
+
+        public NativeList<int> AddTriangles(NativeList<int> triangles)
+        {
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 2);
+            triangles.Add(vertexIndex + 1);
+
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
+            triangles.Add(vertexIndex + 3);
+
+            return triangles;
+        }
+
+
+        public NativeList<Color> AddColors (NativeList<Color> colors) {
+            colors.Add(color1);
+            colors.Add(color1);
+            colors.Add(color2);
+            colors.Add(color2);
+
+            return colors;
+        }
+    }
+}
---
+++ Assets/Scripts/Hex/Services/Render/RenderTriangle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+namespace Hex
+{
+    public class RenderTriangle: ITriangulatable
+    {
+        int vertexIndex;
+        Vector3 vertex1;
+        Vector3 vertex2;
+        Vector3 vertex3;
+        Color color1;
+        Color color2;
+        Color color3;
+
+        public RenderTriangle(
+            int vertexIndex,
+            Vector3 vertex1,
+            Vector3 vertex2,
+            Vector3 vertex3,
+            Color color1,
+            Color color2,
+            Color color3
+        ) {
+            this.vertexIndex = vertexIndex;
+
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.vertex3 = vertex3;
+
+            this.color1 = color1;
+            this.color2 = color2;
+            this.color3 = color3;
+        }
+
+        public NativeList<Vector3> AddVertices(NativeList<Vector3> vertices)
+        {
+            vertices.Add(vertex1);
+            vertices.Add(vertex2);
+            vertices.Add(vertex3);
+
+            return vertices;
+        }
+
+        public NativeList<int> AddTriangles(NativeList<int> triangles)
+        {
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
+
+            return triangles;
+        }
+
+        public NativeList<Color> AddColors(NativeList<Color> colors)
+        {
+            colors.Add(color1);
+            colors.Add(color2);
+            colors.Add(color3);
+
+            return colors;
+        }
+    }
+}
