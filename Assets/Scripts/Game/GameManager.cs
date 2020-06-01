1249ceb60bbab141c47b192872b5d89797629a10
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-01 19:00:33 -0300

    feat: create a game manager mono behaviour
    - initially, it creates a civilization entity

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        void Start()
+        {
+
+            // Create Civilization
+            Civilization civ = new Civilization();
+            civ.Create();
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
