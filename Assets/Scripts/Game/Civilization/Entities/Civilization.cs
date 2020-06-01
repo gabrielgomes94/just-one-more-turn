ab88ced4a7cf6e0f56b726533e04c38a5e576934
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-01 19:04:22 -0300

    feat(civ): add a civ entity class

---
+++ Assets/Scripts/Game/Civilization/Entities/Civilization.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+namespace Game
+{
+    public class Civilization
+    {
+        public void Create()
+        {
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+
+            EntityArchetype archetype = entityManager.CreateArchetype(
+                typeof(Name),
+                typeof(CivColors),
+                typeof(CivCities),
+                typeof(CivUnits)
+            );
+
+            Entity entity = entityManager.CreateEntity(archetype);
+
+            Debug.Log(entity);
+        }
+    }
+}
