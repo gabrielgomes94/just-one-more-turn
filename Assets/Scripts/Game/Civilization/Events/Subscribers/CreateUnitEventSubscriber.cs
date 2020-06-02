b0916be8f51b93457c8205d7a67d9c00306de07b
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-02 20:57:29 -0300

    feat: add event to create unit
    - craete a subscriber and an event args class

---
+++ Assets/Scripts/Game/Civilization/Events/Subscribers/CreateUnitEventSubscriber.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+
+namespace Game
+{
+    public class CreateUnitEventSubscriber : MonoBehaviour {
+        private void Start() {
+            GameManager gameManager = GetComponent<GameManager>();
+            gameManager.OnCivilizationCreated += OnCreateUnitOnCivilizationCreated;
+        }
+
+        protected virtual void OnCreateUnitOnCivilizationCreated(object sender, CreateUnitEventArgs e) {
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            EntityArchetype createUnitEventArch = entityManager.CreateArchetype(
+                typeof(CreateUnitEventComponent)
+            );
+
+            Entity entity = entityManager.CreateEntity(createUnitEventArch);
+
+            entityManager.SetComponentData(
+                entity,
+                new CreateUnitEventComponent {
+                    position = e.position
+                }
+            );
+        }
+    }
+}
