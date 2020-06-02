b384a119e7a935b0fee769563f359ec889eaab39
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-02 20:54:49 -0300

    feat(game): add game manager class
    - this class will serve to control the game state

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,22 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 namespace Game
 {
     public class GameManager : MonoBehaviour
     {
+        public event EventHandler<CreateUnitEventArgs> OnCivilizationCreated;
+
         void Start()
         {
-
             // Create Civilization
             Civilization civ = new Civilization();
             civ.Create();
-        }
 
-        void Update()
-        {
+            // Create Civilization's settler
+            CreateUnitEventArgs args = new CreateUnitEventArgs();
+            args.position = new float3(10, 20, 30);
 
+
+            OnCivilizationCreated?.Invoke(this, args);
         }
     }
 }
