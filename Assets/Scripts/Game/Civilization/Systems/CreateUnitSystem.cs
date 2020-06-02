86a933f56cc67fdf57c43100da550798b886e1b0
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-02 20:58:39 -0300

    feat: add system to create units
    - for now, it only creates settler

---
+++ Assets/Scripts/Game/Civilization/Systems/CreateUnitSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+namespace Game
+{
+    public class CreateUnitSystem : SystemBase
+    {
+        EndSimulationEntityCommandBufferSystem barrier => World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+        protected override void OnUpdate()
+        {
+            var ecb = barrier.CreateCommandBuffer();
+            SettlerPrefab settlerPrefab = GetSingleton<SettlerPrefab>();
+
+            Entities
+                .ForEach((
+                    Entity entity,
+                    int entityInQueryIndex,
+                    ref CreateUnitEventComponent createUnitEvent
+                ) => {
+                    Entity settler = ecb.Instantiate(settlerPrefab.Value);
+
+                    ecb.SetComponent<Translation>(settler, new Translation { Value = createUnitEvent.position });
+
+                    ecb.DestroyEntity(entity);
+                })
+                .Schedule();
+
+            barrier.AddJobHandleForProducer(this.Dependency);
+        }
+    }
+}
