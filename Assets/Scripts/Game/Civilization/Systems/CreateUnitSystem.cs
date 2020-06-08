9f95bf55788fa6c7a5b689186a3ff1cdf3271dd9
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-08 11:57:59 -0300

    feat: add collider to settler entity

--- Assets/Scripts/Game/Civilization/Systems/CreateUnitSystem.cs
+++ Assets/Scripts/Game/Civilization/Systems/CreateUnitSystem.cs
@@ -5,6 +5,7 @@ using Unity.Entities;
 using Unity.Jobs;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Physics;
 
 namespace Game
 {
@@ -26,8 +27,19 @@ namespace Game
                 ) => {
                     Entity settler = ecb.Instantiate(settlerPrefab.Value);
 
+                    BoxGeometry settlerGeometry = new BoxGeometry
+                    {
+                        Center = float3.zero,
+                        Orientation = quaternion.identity,
+                        Size = new float3(3.0f, 3.0f, 3.0f),
+                        BevelRadius = 0.01f
+                    };
+
+                    BlobAssetReference<Unity.Physics.Collider> collider = Unity.Physics.BoxCollider.Create(settlerGeometry);
+
                     ecb.SetComponent<Translation>(settler, new Translation { Value = createUnitEvent.position });
                     ecb.AddSharedComponent<CivIdSharedComponent>(settler, new CivIdSharedComponent { Value = 1} );
+                    ecb.AddComponent<PhysicsCollider>(settler, new PhysicsCollider { Value = collider });
                     ecb.DestroyEntity(entity);
                 })
                 .Run();
