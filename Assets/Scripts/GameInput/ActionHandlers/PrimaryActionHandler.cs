8151a2360bb3b84678cebe89d6c18bc9a226abb1
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-09 20:10:23 -0300

    fix: add new unity dots raycast for primary input
    - also, selects the entity

--- Assets/Scripts/GameInput/ActionHandlers/PrimaryActionHandler.cs
+++ Assets/Scripts/GameInput/ActionHandlers/PrimaryActionHandler.cs
@@ -1,6 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using Unity.Collections;
+using System;
+using Interaction;
+using Game;
 
 namespace GameInput
 {
@@ -8,15 +16,48 @@ namespace GameInput
     {
         public void handle(Vector3 position)
         {
-            Ray inputRay = Camera.main.ScreenPointToRay(position);
-            Debug.Log(inputRay);
-            RaycastHit hit;
+            UnityEngine.Ray inputRay = Camera.main.ScreenPointToRay(position);
 
-            if (Physics.Raycast(inputRay, out hit)) {
-                Debug.Log("tocou");
-                IInteractable interfaceInteractable = hit.transform.gameObject.GetComponent<IInteractable>();
-                interfaceInteractable.Select();
+            var entityRaycasted = Raycast(inputRay.origin, inputRay.direction * 200f);
+
+            if (entityRaycasted == Entity.Null) return;
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            var query = entityManager.CreateEntityQuery(ComponentType.ReadOnly<Selected>());
+
+            if (query.CalculateEntityCount() > 0 )
+            {
+                entityManager.RemoveComponent(query, typeof(Selected));
             }
+
+            entityManager.AddComponentData<Selected>(entityRaycasted, new Selected{});
+        }
+
+        public Entity Raycast(float3 RayFrom, float3 RayTo)
+        {
+            BuildPhysicsWorld physicsWorldSystem = World.DefaultGameObjectInjectionWorld.GetExistingSystem<Unity.Physics.Systems.BuildPhysicsWorld>();
+            var collisionWorld = physicsWorldSystem.PhysicsWorld.CollisionWorld;
+
+            RaycastInput input = new RaycastInput()
+            {
+                Start = RayFrom,
+                End = RayTo,
+                Filter = new CollisionFilter()
+                {
+                    BelongsTo = ~0u,
+                    CollidesWith = ~0u,
+                    GroupIndex = 0
+                }
+            };
+
+            Unity.Physics.RaycastHit hit = new Unity.Physics.RaycastHit();
+            bool haveHit = collisionWorld.CastRay(input, out hit);
+            if (haveHit)
+            {
+                Entity e = physicsWorldSystem.PhysicsWorld.Bodies[hit.RigidBodyIndex].Entity;
+                return e;
+            }
+            return Entity.Null;
         }
     }
 }
