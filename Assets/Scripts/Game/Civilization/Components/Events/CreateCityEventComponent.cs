132253c3e3e021181fb3b975fc6cad6f08fb3ae9
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-11 20:47:42 -0300

    feat: add `create city event` component

---
+++ Assets/Scripts/Game/Civilization/Components/Events/CreateCityEventComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct CreateCityEventComponent : IComponentData
+{
+    public int3 Coordinates;
+}
