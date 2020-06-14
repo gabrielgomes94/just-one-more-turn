21b329034e4d9a6b5becc73701af65cf8bb71513
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-14 15:57:18 -0300

    refact: organize create unit system
    - uses  the settler API to create a settler

---
+++ Assets/Scripts/Game/Unit/Systems/CreateUnitSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+using Hex;
+
+namespace Game
+{
+    public class CreateUnitSystem : SystemBase
+    {
+        EndSimulationEntityCommandBufferSystem barrier => World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+        protected override void OnUpdate()
+        {
+            EntityCommandBuffer ecb = barrier.CreateCommandBuffer();
+            SettlerPrefab settlerPrefab = GetSingleton<SettlerPrefab>();
+
+            Entities
+                .WithoutBurst()
+                .ForEach((
+                    Entity entity,
+                    int entityInQueryIndex,
+                    in CommandCreateUnitComponent createUnitCommand
+                ) => {
+                    int3 coordinates = createUnitCommand.Coordinates;
+
+                    SettlerEntity.Create(ecb, settlerPrefab, coordinates);
+
+                    ecb.DestroyEntity(entity);
+                })
+                .Run();
+
+            barrier.AddJobHandleForProducer(this.Dependency);
+        }
+    }
+}
