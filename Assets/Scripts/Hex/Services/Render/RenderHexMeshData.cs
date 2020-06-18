f3f7796f5c46989c4229c58805f4c16fd7f95517
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-18 18:43:54 -0300

    feat(hex): add a render hex mesh data class

---
+++ Assets/Scripts/Hex/Services/Render/RenderHexMeshData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Hex
+{
+    public class RenderHexMeshData
+    {
+        public List<int> triangles;
+        public List<Vector3> vertices;
+        public List<Color> colors;
+
+        public RenderHexMeshData()
+        {
+            this.triangles = new List<int>();
+            this.vertices = new List<Vector3>();
+            this.colors = new List<Color>();
+        }
+    }
+}
