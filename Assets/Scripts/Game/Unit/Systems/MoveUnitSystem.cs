4ebfdb9267ec19540ef410bd13326d4fad24c7e4
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-18 01:07:03 -0300

    feat: create move unit system

---
+++ Assets/Scripts/Game/Unit/Systems/MoveUnitSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Hex;
+
+namespace Game
+{
+    public class MoveUnitSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities
+                .ForEach((
+                    Entity entity,
+                    int entityInQueryIndex,
+                    ref Translation translation,
+                    in Movable movable,
+                    in CommandMoveComponent cmdMove
+                ) => {
+                    translation.Value =  HexCell.GetTranslationComponentByHexCoordinates(
+                        new HexCoordinates{ Value = new int3(cmdMove.moveToCoordinates) }
+                    );
+
+                    // EntityManager.RemoveComponent<CommandMoveComponent>(entity);
+                })
+                .Schedule();
+        }
+    }
+}
