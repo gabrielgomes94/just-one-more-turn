9f97e5095197c52386c5d08690a7ca9c99d3671b
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-28 09:24:57 -0300

    feat: add text coordinates label on hex grid

--- Assets/Scripts/Hex/HexGrid.cs
+++ Assets/Scripts/Hex/HexGrid.cs
@@ -12,7 +12,6 @@ namespace Hex
 
         public Text cellLabelPrefab;
         Canvas gridCanvas;
-        HexMesh hexMesh;
 
         void Awake()
         {
@@ -23,6 +22,28 @@ namespace Hex
         {
             HexCellEntity cellEntity = new HexCellEntity();
             cellEntity.CreateCells(width, height);
+
+            AddTextCoordinatesOnGrid(width, height);
+        }
+
+        void AddTextCoordinatesOnGrid(int width, int height)
+        {
+            for (int z = 0, i = 0; z < height; z++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Text label = Instantiate<Text>(cellLabelPrefab);
+
+                    label.rectTransform.SetParent(gridCanvas.transform, false);
+                    label.rectTransform.anchoredPosition = new Vector2(
+                        PositionCalculator.GetPositionX(x, z),
+                        PositionCalculator.GetPositionZ(z)
+                    );
+
+                    HexCoordinates coordinates = CoordinatesService.CreateFromOffset(x, z);
+                    label.text = "" + coordinates.Value.x + ", " + coordinates.Value.y + ", " + coordinates.Value.z + "";
+                }
+            }
         }
     }
 }
