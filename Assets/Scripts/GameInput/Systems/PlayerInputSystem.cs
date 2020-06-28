998d5171009580ba50fcb6c6cf1ce45ec7070b96
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-06-28 20:34:15 -0300

    refactor: change method to enhance readability

--- Assets/Scripts/GameInput/Systems/PlayerInputSystem.cs
+++ Assets/Scripts/GameInput/Systems/PlayerInputSystem.cs
@@ -21,13 +21,12 @@ namespace GameInput
                 EntityManager.CreateEntity(typeof(MouseInput));
 
             if (Input.GetMouseButton(0)) {
-                Debug.Log(Input.mousePosition);
+                var position = new float3(Input.mousePosition.x, Input.mousePosition.y, 0f);
 
-                mouseInputQuery.SetSingleton(new MouseInput
-                {
+                mouseInputQuery.SetSingleton(new MouseInput {
                     primaryAction = 1,
                     secondaryAction = 0,
-                    mousePosition = new float3(Input.mousePosition.x, Input.mousePosition.y, 0f)
+                    mousePosition = position
                 });
             }
         }
