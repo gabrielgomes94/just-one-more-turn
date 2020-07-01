1aac3e7c4c88c3ac4ac2132bdf92ec54587cf246
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-07-01 02:43:25 -0300

    test: fix render quad tests

--- Assets/Tests/EditorMode/Hex/Services/Render/Operations/RenderQuadTest.cs
+++ Assets/Tests/EditorMode/Hex/Services/Render/Operations/RenderQuadTest.cs
@@ -11,10 +11,10 @@ namespace Tests
     public class RenderQuadTest
     {
         [Test]
-        public void Test_Should_Add_Vertices_To_NativeList()
+        public void Test_Should_Add_Vertices_To_List()
         {
             // Set
-            NativeList<Vector3> vertices = new NativeList<Vector3>(Allocator.TempJob);
+            List<Vector3> vertices = new List<Vector3>();
 
             Vector3 vertex1 = new Vector3(0f, 0f, 0f);
             Vector3 vertex2 = new Vector3(0f, 0f, 2f);
@@ -24,23 +24,16 @@ namespace Tests
             Color color1 = Color.black;
             Color color2 = Color.black;
 
-            NativeArray<Vector3> quadVertices = new NativeArray<Vector3>(
-                new Vector3[] { vertex1, vertex2, vertex3, vertex4,
-                },
-                Allocator.Temp
-            );
+            Vector3[] quadVertices = new Vector3[] { vertex1, vertex2, vertex3, vertex4 } ;
 
-            NativeArray<Color> quadColors = new NativeArray<Color>(
-                new Color[] { color1, color2 },
-                Allocator.Temp
-            );
+            Color[] quadColors = new Color[] { color1, color2 } ;
 
             int vertexIndex = 3;
 
             RenderQuad renderQuad = new RenderQuad(vertexIndex, quadVertices, quadColors);
 
             // Act
-            NativeList<Vector3> verticesResult = renderQuad.AddVertices(vertices);
+            List<Vector3> verticesResult = renderQuad.AddVertices(vertices);
 
             // Assert
             Assert.AreEqual(
@@ -62,18 +55,13 @@ namespace Tests
                 new Vector3(2f, 0f, 2f),
                 verticesResult.ToArray()[3]
             );
-
-            // Unset
-            vertices.Dispose();
-            quadVertices.Dispose();
-            quadColors.Dispose();
         }
 
         [Test]
-        public void Test_Should_Add_Triangles_To_NativeList()
+        public void Test_Should_Add_Triangles_To_List()
         {
             // Set
-            NativeList<int> triangles = new NativeList<int>(Allocator.TempJob);
+            List<int> triangles = new List<int>();
 
             Vector3 vertex1 = new Vector3(0f, 0f, 0f);
             Vector3 vertex2 = new Vector3(0f, 0f, 2f);
@@ -83,23 +71,16 @@ namespace Tests
             Color color1 = Color.black;
             Color color2 = Color.black;
 
-            NativeArray<Vector3> quadVertices = new NativeArray<Vector3>(
-                new Vector3[] { vertex1, vertex2, vertex3, vertex4,
-                },
-                Allocator.TempJob
-            );
+            Vector3[] quadVertices = new Vector3[] { vertex1, vertex2, vertex3, vertex4 } ;
 
-            NativeArray<Color> quadColors = new NativeArray<Color>(
-                new Color[] { color1, color2 },
-                Allocator.TempJob
-            );
+            Color[] quadColors = new Color[] { color1, color2 } ;
 
             int vertexIndex = 1;
 
             RenderQuad renderQuad = new RenderQuad(vertexIndex, quadVertices, quadColors);
 
             // Act
-            NativeList<int> trianglesResult = renderQuad.AddTriangles(triangles);
+            List<int> trianglesResult = renderQuad.AddTriangles(triangles);
 
             // Assert
             Assert.AreEqual(1, trianglesResult[0]);
@@ -108,18 +89,13 @@ namespace Tests
             Assert.AreEqual(2, trianglesResult[3]);
             Assert.AreEqual(3, trianglesResult[4]);
             Assert.AreEqual(4, trianglesResult[5]);
-
-            // Unset
-            triangles.Dispose();
-            quadVertices.Dispose();
-            quadColors.Dispose();
         }
 
         [Test]
-        public void Test_Should_Add_Colors_To_NativeList()
+        public void Test_Should_Add_Colors_To_List()
         {
             // Set
-            NativeList<Color> colors = new NativeList<Color>(Allocator.TempJob);
+            List<Color> colors = new List<Color>();
 
             Vector3 vertex1 = new Vector3(0f, 0f, 0f);
             Vector3 vertex2 = new Vector3(0f, 0f, 2f);
@@ -129,34 +105,22 @@ namespace Tests
             Color color1 = Color.black;
             Color color2 = Color.white;
 
-            NativeArray<Vector3> quadVertices = new NativeArray<Vector3>(
-                new Vector3[] { vertex1, vertex2, vertex3, vertex4,
-                },
-                Allocator.TempJob
-            );
+            Vector3[] quadVertices = new Vector3[] { vertex1, vertex2, vertex3, vertex4 } ;
 
-            NativeArray<Color> quadColors = new NativeArray<Color>(
-                new Color[] { color1, color2 },
-                Allocator.TempJob
-            );
+            Color[] quadColors = new Color[] { color1, color2 } ;
 
             int vertexIndex = 1;
 
             RenderQuad renderQuad = new RenderQuad(vertexIndex, quadVertices, quadColors);
 
             // Act
-            NativeList<Color> colorsResult = renderQuad.AddColors(colors);
+            List<Color> colorsResult = renderQuad.AddColors(colors);
 
             // Assert
             Assert.AreEqual(Color.black, colorsResult[0]);
             Assert.AreEqual(Color.black, colorsResult[1]);
             Assert.AreEqual(Color.white, colorsResult[2]);
             Assert.AreEqual(Color.white, colorsResult[3]);
-
-            // Unset
-            colors.Dispose();
-            quadVertices.Dispose();
-            quadColors.Dispose();
         }
     }
 }
