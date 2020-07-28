a6687680d69c88fb5fc8a563c48b5e5e0b4968ef
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-07-28 11:24:02 -0300

    feat: add class to create and remove command highlights

---
+++ Assets/Scripts/Game/Map/HighlightCell/Services/CommandHighlight.cs
@@ -0,0 +1,24 @@
+using Unity.Entities;
+using Hex;
+
+namespace Game
+{
+    public class CommandHighlight
+    {
+        public static void Create(EntityCommandBuffer ecb, HexCoordinates coordinates, EntityArchetype archetype)
+        {
+            Entity entity = ecb.CreateEntity(archetype);
+
+            ecb.AddComponent<CommandCreateHighlight>(entity, new CommandCreateHighlight {});
+            ecb.AddComponent<HexCoordinates>(entity, coordinates);
+        }
+
+        public static void Remove(EntityCommandBuffer ecb, HexCoordinates coordinates, EntityArchetype archetype)
+        {
+            Entity entity = ecb.CreateEntity(archetype);
+
+            ecb.AddComponent<CommandRemoveHighlight>(entity, new CommandRemoveHighlight {});
+            ecb.AddComponent<HexCoordinates>(entity, coordinates);
+        }
+    }
+}
