8bdb4f4d26e20f16820e7da03438af1c0893c6dc
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-08-11 11:55:12 -0300

    feat: instantiate city panel prefab

---
+++ Assets/Scripts/Game/UI/CityPanelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Hex;
+
+public class CityPanelManager : MonoBehaviour
+{
+    public GameObject cityPanelPrefab;
+    Canvas gridCanvas;
+
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+    }
+
+    void Start()
+    {
+        GameObject label = Instantiate(cityPanelPrefab);
+        label.GetComponent<RectTransform>().SetParent(gridCanvas.transform, false);
+        label.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(
+            PositionCalculator.GetPositionX(17, 8),
+            PositionCalculator.GetPositionZ(8),
+            220f
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
