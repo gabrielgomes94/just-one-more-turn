5394487663d1658cf1a6719136edd90b95f4733c
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-08-16 16:52:29 -0300

    feat: add settler panel manager

---
+++ Assets/Scripts/GameUI/Managers/Settler/SettlerPanelManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Hex.Coordinates;
+using GameUI.Events;
+
+namespace GameUI.Managers
+{
+    public class SettlerPanelManager : MonoBehaviour
+    {
+        public GameObject settlerPanelPrefab;
+        EntityManager entityManager;
+        EntityQuery query;
+
+        void Awake()
+        {
+            entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            query = entityManager.CreateEntityQuery(
+                ComponentType.ReadOnly<SettlerPanelTag>(),
+                ComponentType.ReadOnly<HexCoordinates>()
+            );
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        void LateUpdate()
+        {
+            NativeArray<Entity> panels = query.ToEntityArray(Allocator.TempJob);
+
+            foreach(var panel in panels)
+            {
+                settlerPanelPrefab.SetActive(true);
+            }
+
+            panels.Dispose();
+        }
+    }
+}
