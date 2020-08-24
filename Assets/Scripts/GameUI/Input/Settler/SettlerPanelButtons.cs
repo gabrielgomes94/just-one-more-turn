89395f039cae5069d68c63ce9e8a889ec9bdb657
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-08-24 15:52:50 -0300

    feat(UI): create input UI directory for game

---
+++ Assets/Scripts/GameUI/Input/Settler/SettlerPanelButtons.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Hex.Coordinates;
+using Game;
+
+public class SettlerPanelButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CreateCity()
+        {
+            Debug.Log("Criação de cidade");
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            Entity entity = SettlerEntity.GetSelected(entityManager);
+
+            SettlerEntity.AddCommandCreateCity(entityManager, entity);
+
+            // settlerPanelPrefab.SetActive(false);
+
+            // if (panel != Entity.Null) {
+            //     entityManager.DestroyEntity(panel);
+            // }
+        }
+
+        public void BackButton()
+        {
+            Debug.Log("Destrua o painel");
+            // settlerPanelPrefab.SetActive(false);
+        }
+}
