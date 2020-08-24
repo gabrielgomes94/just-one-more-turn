51a037e2cab3398ccff7d040fb9cd3cfd7ee38a9
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-08-24 15:47:58 -0300

    feat(UI): add city label for gameUI view

---
+++ Assets/Scripts/GameUI/View/City/CityLabel.cs
@@ -0,0 +1,12 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace GameUI.View
+{
+    public class CityLabel {
+        public static void Create(Entity city)
+        {
+            Debug.Log(city);
+        }
+    }
+}
