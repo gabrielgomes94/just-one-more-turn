725c857f38ba1ccf81f999ad15b2071d515300ac
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-09-07 15:27:20 -0300

    feat(UI): show cities' label

--- Assets/Scripts/GameUI/View/City/CityLabelManager.cs
+++ Assets/Scripts/GameUI/View/City/CityLabelManager.cs
@@ -1,36 +1,46 @@
-﻿using Unity.Entities;
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 using Hex.Cell;
 using Hex.Coordinates;
+using GameUI.Entities;
 
-namespace GameUI.Components
+namespace GameUI.View
 {
     public class CityLabelManager : MonoBehaviour
     {
         public GameObject cityPanelPrefab;
         Canvas gridCanvas;
-
+        EntityManager entityManager;
         EntityQuery query;
 
         void Awake()
         {
-            gridCanvas = GetComponentInChildren<Canvas>();
+            entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            gridCanvas = GetComponentInParent<Canvas>();
         }
 
-        void Start()
+        void LateUpdate()
         {
-            GameObject label = Instantiate(cityPanelPrefab);
-            var pos = HexCellService.GetTranslationComponentByHexCoordinates(CoordinatesService.CreateFromOffset(8, 2));
+            query = CityLabel.GetQuery();
 
-            label.GetComponent<RectTransform>().SetParent(gridCanvas.transform, false);
-            label.GetComponent<RectTransform>().position = new Vector3(pos.x, pos.y + 10f, pos.z - 5f);
-        }
+            if (query.CalculateEntityCount() > 0) {
+                NativeArray<Entity> labels = query.ToEntityArray(Allocator.TempJob);
 
-        // Update is called once per frame
-        void Update()
-        {
+                foreach(Entity cityLabel in labels) {
+                    GameObject label = Instantiate(cityPanelPrefab);
+                    float3 pos = CityLabel.GetWorldPosition(cityLabel);
 
+                    label.GetComponent<RectTransform>().SetParent(gridCanvas.transform, false);
+                    label.GetComponent<RectTransform>().position = new Vector3(pos.x, pos.y + 10f, pos.z - 5f);
+
+                    entityManager.RemoveComponent<UICreate>(cityLabel);
+                }
+
+                labels.Dispose();
+            }
         }
     }
 }
