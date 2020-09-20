73fceb8f79326b80b7cbd0f0db097666af1117e9
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-09-20 13:52:14 -0300

    reface(ui): rename view namespace

---
+++ Assets/Scripts/GameUI/Views/CityLabel/CityLabel.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Entities;
+using Unity.Mathematics;
+using GameUI.Models;
+using System;
+
+namespace GameUI.View
+{
+    public class CityLabel
+    {
+        public string name;
+        public string population;
+        public GameObject label;
+
+        public CityLabel(GameObject label, CityData cityData)
+        {
+            this.label = label;
+            this.name = cityData.name;
+            this.population = cityData.population.ToString();
+        }
+
+        public void SetButton()
+        {
+            CityLabelButton cityLabelButton = this.label.GetComponentInChildren<CityLabelButton>();
+            cityLabelButton.cityData = new CityData(name, Int32.Parse(population));
+        }
+
+        public void SetContent(GameObject label)
+        {
+            GameObject cityName = label.transform.Find("City Name").gameObject;
+            GameObject cityPopulation = label.transform.Find("City Population").gameObject;
+
+            cityName.GetComponent<Text>().text = name;
+            cityPopulation.GetComponent<Text>().text = population.ToString();
+        }
+        public void SetPosition(Entity cityLabel, Canvas gridCanvas)
+        {
+            float3 pos = CityLabelECS.GetWorldPosition(cityLabel);
+
+            this.label.GetComponent<RectTransform>().SetParent(gridCanvas.transform, false);
+            this.label.GetComponent<RectTransform>().position = new Vector3(pos.x, pos.y + 10f, pos.z - 5f);
+        }
+    }
+}
