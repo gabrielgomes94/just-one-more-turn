869c591f831c5c553a182cd68bf4caf0008e4353
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-09-22 00:59:27 -0300

    refact(ui): remove boilerplate

--- Assets/Scripts/GameUI/Views/Settler/SettlerPanelButtons.cs
+++ Assets/Scripts/GameUI/Views/Settler/SettlerPanelButtons.cs
@@ -13,34 +13,17 @@ namespace GameUI.View
 {
     public class SettlerPanelButtons : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
         public void CreateCity()
         {
-            Debug.Log("Criação de cidade");
             EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
             Entity entity = SettlerRepository.GetSelected(entityManager);
 
             SettlerService.AddCommandCreateCity(entityManager, entity);
-
             UISettlerPanel.Hide();
         }
 
         public void BackButton()
         {
-            Debug.Log("Destrua o painel");
-
             UISettlerPanel.Hide();
         }
     }
